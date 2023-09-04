bfe528f18bdd1048d8a543207d5ef81ca72c4b51
Ducker5839 <143706029+Ducker5839@users.noreply.github.com>
2023-09-04 11:47:15 +0300

    1

--- main.gd
+++ main.gd
@@ -3,13 +3,12 @@ extends Node2D
 @onready var arrow = $arrow
 
 var animalPosition
-#var animalName = ["bee", "spider"]
 var animalHome = ["beehive", "cobweb"]
 var animalName
 var matches = {"bee": "beehive", "spider": "cobweb"}
 var home
 
-#func _on_click_check(firstClick, animal, pos, event, shape_idx):
-#	animalName = animal
-#	animalPosition = animal.global_position
-#	arrow.position = Vector2(animal.global_position.x, animal.global_position.y - 120)
+func _on_click_check(firstClick, animal, pos, event, shape_idx):
+	animalName = animal
+	animalPosition = animal.global_position
+	arrow.position = Vector2(animal.global_position.x, animal.global_position.y - 120)
